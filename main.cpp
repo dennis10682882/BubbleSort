16e49bed704d22a9d55d2e3a5838efeb2eca13c3
cobiDP <42798959+dennis10682882@users.noreply.github.com>
2020-03-11 01:52:02 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+// C++ program to implement Bubble sort algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *d, int *p)
+{
+    int temp = *d;
+    *d = *p;
+    *p = temp;
+}
+
+
+void bSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)
+
+
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+}
+
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+
+int main()
+{
+    int arr[] = {10, 2, 8, 20, 22, 1, 100, 3, 6, 50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bSort(arr, n);
+    cout<<"Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
